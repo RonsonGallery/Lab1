477d90d152ed0fdec1f5dbaa731daba5cb6ed6d8
Owner <ronson537@gmail.com>
2020-11-18 19:43:43 +0200

    added gauss zidel jacobi methods for solving a quadratic matrix

---
+++ Gauss-zidel.py
@@ -0,0 +1,53 @@
+def gaus_zidel(matrix,b):
+    xn = 0
+    yn = 0
+    zn = 0
+    epsilon = 0.0001
+    Iteration = 0
+    for Iteration in range (100):
+        xn_1 =   (b[0] - matrix[0][1]*yn - matrix[0][2]*zn) / matrix[0][0]
+        Yn_1 =   (b[1] - matrix[1][0]*xn_1 - matrix[1][2]*zn) / matrix[1][1]
+        Zn_1 =   (b[2] - matrix[2][0] * xn_1 - matrix[2][1] * Yn_1) / matrix[2][2]
+        print("gaus_zidel Iteration number:",Iteration,xn_1," ",Yn_1," ",Zn_1)
+        if xn_1 - xn == 0 and Yn_1 - yn == 0 and Zn_1 - zn ==0:
+            break
+        if xn_1 - xn < epsilon:
+            xn_1 = round(xn_1,3)
+        if Yn_1 - yn < epsilon:
+            Yn_1=round(Yn_1, 3)
+        if Zn_1 - zn < epsilon:
+            Zn_1 = round(Zn_1, 3)
+        xn = xn_1
+        yn = Yn_1
+        zn = Zn_1
+
+def jacobi(matrix,b):
+    xn = 0
+    yn = 0
+    zn = 0
+    epsilon = 0.0001
+    Iteration = 0
+    for Iteration in range(100):
+        xn_1 = (b[0] - matrix[0][1] * yn - matrix[0][2] * zn) / matrix[0][0]
+        Yn_1 = (b[1] - matrix[1][0] * xn - matrix[1][2] * zn) / matrix[1][1]
+        Zn_1 = (b[2] - matrix[2][0] * xn - matrix[2][1] * yn) / matrix[2][2]
+        print("jacobi Iteration number:", Iteration, xn_1, " ", Yn_1, " ", Zn_1)
+        if xn_1 - xn == 0 and Yn_1 - yn == 0 and Zn_1 - zn == 0:
+            break
+        if xn_1 - xn < epsilon:
+            xn_1 = round(xn_1, 3)
+        if Yn_1 - yn < epsilon:
+            Yn_1 = round(Yn_1, 3)
+        if Zn_1 - zn < epsilon:
+            Zn_1 = round(Zn_1, 3)
+        xn = xn_1
+        yn = Yn_1
+        zn = Zn_1
+
+
+matrix = [[3,-1,1],[0,1,-1],[1,1,-2]]
+b = [4,-1,-3]
+#matrix2 = [[3,-1,1],[0,1,-1],[1,1,-2]]
+#b2 = [4,-1,-3]
+gaus_zidel(matrix,b)
+jacobi(matrix,b)
