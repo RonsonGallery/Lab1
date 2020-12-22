30fd403142b5fae0ee5c7a2544b237a2097fa50f
Owner <ronson537@gmail.com>
2020-12-22 17:49:12 +0200

    added bisection method

--- Secant.py
+++ Secant.py
@@ -21,11 +21,9 @@ def secant(p0, p1):
         #print('Itteration:',i , ' - ', p)
         if abs(p - p1) < epsilon:
             return p
-        print("{0:.4f}              {1:.4f}                 {2:.4f}                 {3:.4f}                 {4:.4f}             {5:.4f}             {6:.4f}".format(i,p0,function(p0),p1,function(p1),p,function(p)))
         p0 = p1
         p1 = p
 
     return "Them method failed after {} iterations" > format(MAX)
 
-print("iteration            x0                     f(x0)                   x1                    f(x1)                x2                 f(x2)")
 print("Secant method solution: x = ", secant(2, 1))
