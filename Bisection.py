22bf8e1d281357d53219d464d7440cac58dc453c
Owner <ronson537@gmail.com>
2021-01-18 15:36:48 +0200

    Added some sidesteps

--- Bisection.py
+++ Bisection.py
@@ -1,6 +1,6 @@
 #Roni Gerkerov - 316583145
 #Eden Mozes - 315997049
-
+import math
 
 import sympy as sp
 from numpy import log
@@ -12,12 +12,14 @@ def func_calc(func,x,val):
 
 def f(x):
     #x = sp.symbols('x')
-    return x**4 + x**3 - 3*x**2
+    #x**5 -5*x - +2
+    #(math.sin(x**2 + 5*x + 6))/(2*(2.718)**(-x))
+    return (0.5*2.2718**x)*(math.sin(x**2 + 5*x + 6))
 
 def bisection (a,b,epsilon):                  #Recives a range and returns the root in that range if one exists
     xl = a
     xr = b
-    k = round(-log(epsilon/(b-a))/log(2)) + 1
+    #k = round(-log(epsilon/(b-a))/log(2)) + 1
     iterator = 0
     #print(k)
     while abs((xl-xr)) >= epsilon:
@@ -27,8 +29,8 @@ def bisection (a,b,epsilon):                  #Recives a range and returns the r
             xl = c
         elif prod < epsilon:
             xr = c
-        if iterator == int(k):
-            return None
+        #if iterator == int(k):
+            #return None
         iterator += 1
     return c
 
@@ -43,7 +45,7 @@ def bisection_special (a,b,epsilon,f):                     #same as the previos 
         elif prod < epsilon:
             xr = c
     return c
-
+iter = 0
 def bigger_bisection(a,b,g):                             #recives a large range and uses bisection function to find all possible roots
     xl = a
     xr = b
@@ -63,7 +65,7 @@ def bigger_bisection(a,b,g):                             #recives a large range 
         #print(my_dict[key])
         if current * last < 0:
             #print(current, last)
-            #print(current_key,last_key)
+            print("Potential root in this range: " + str( current_key), str(last_key))
             if bisection(current_key, last_key, 1e-10) != None:
                 roots.append(bisection(current_key, last_key, 1e-10))
         last = current
@@ -74,7 +76,7 @@ def bigger_bisection_diff(a,b,g):                                   #same as pre
     xr = b
     my_dict = {}
     while xr - 0.1 >= xl - 0.1:
-        #    print(xr)
+        #print(xr)
         my_dict[xr] = func_calc(g,x,xr)
         xr = round(xr - 0.100, 2)
     current = 0
@@ -121,15 +123,19 @@ def check_root(x):
     if f(x) == 0:
         return True
     return False
-
+def ftx(x):
+    return 0.5 * (2.718 ** x) * (math.cos(x**2 + 6 + 5*x)*(5+2*x) + math.sin(x**2 + 6 + 5*x))
 #print(key , current)
 #answer = bisection(-5,5,1e-10)
 a = -3
-b = 2
+b = 1
 x = sp.symbols('x')
 solutions = bigger_bisection(a,b,f)
-ftx = (sp.diff(f(x),x))
-check_roots = bigger_bisection_diff(a,b,ftx)
+print(solutions)
+#ftx = (sp.diff(f(x),x))
+#check_roots = bigger_bisection_diff(a,b,ftx)
+print("Potential roots for derivative:")
+check_roots = bigger_bisection(a,b,ftx)
 roots = list(filter(check_root,check_roots))
 for i in roots:
     solutions.append(roots[i])
--- Gauss-zidel.py
+++ Gauss-zidel.py
@@ -1,3 +1,6 @@
+#Roni Gerkerov - 316583145
+#Eden Mozes - 315997049
+
 def gaus_zidel(matrix,b):
     xn = 0
     yn = 0
@@ -50,7 +53,39 @@ def dominant_pivot(matrix):
     print("Not a dominant pivot")
     return False
 
+def switcharu(matrix):
+    while not(dominant_pivot(matrix)):
+        if matrix[0][0] < matrix[1][0]:
+            temp = matrix[0]
+            matrix[0] = matrix[1]
+            matrix[1] = temp
+
+        elif matrix[0][0] < matrix[2][0]:
+            temp = matrix[0]
+            matrix[0] = matrix[2]
+            matrix[2] = temp
+
+        if matrix[1][1] < matrix[0][1]:
+            temp = matrix[1]
+            matrix[1] = matrix[0]
+            matrix[0] = temp
+        elif matrix[1][1] < matrix[2][1]:
+            temp = matrix[1]
+            matrix[1] = matrix[2]
+            matrix[2] = temp
+
+        if matrix[2][2] < matrix[0][2]:
+            temp = matrix[2]
+            matrix[2] = matrix[0]
+            matrix[0] = temp
+        elif matrix[2][2] < matrix[1][2]:
+            temp = matrix[2]
+            matrix[2] = matrix[1]
+            matrix[1] = temp
+
+
 matrix = [[3,-1,1],[0,1,-1],[1,1,-2]]
+print(matrix)
 b = [4,-1,-3]
 #matrix2 = [[3,-1,1],[0,1,-1],[1,1,-2]]
 #b2 = [4,-1,-3]
